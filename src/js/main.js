27615c60d4ea0a2c34d80a32f85b193fb353b37d
razumeyka <33911265+razumeyka@users.noreply.github.com>
2019-01-16 18:43:27 +0200

    almost ready

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+@@include('./lib/jquery.fancybox.min.js')
+
+$(document).ready(function(){
+    
+// mobile_menu
+    
+    $('.burger').click( function() { 
+        $('header .menu').slideToggle(300);
+        $('.burger').toggleClass( 'burger_active' ); 
+    });
+    
+// services-hover
+	
+	$('.services__item').mouseenter(function () { 
+		if(window.innerWidth<1280) return;
+		$(this).addClass('services__item_active'); 
+	});
+	
+	$('.services__item').mouseleave(function () { 
+		if(window.innerWidth<1280) return;
+		$(this).removeClass('services__item_active'); 
+	});
+
+//accordeon
+
+ $(".accordion__full").hide();
+    $('.accordion__spoiler').click(function(){
+        $(this).next().slideToggle(300);
+        $(this).toggleClass('accordion__spoiler_active');
+    });
+}); 
